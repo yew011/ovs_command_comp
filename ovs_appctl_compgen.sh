1bf7cdbf00628b64cfd909594dc3ac2d0e9263a6
Alex Wang <alexw@nicira.com>
2014-05-18 18:26:44 -0700

    ovs_appctl_compgen: Makes the ovs-appctl compgen script generic.
    This commit makes the completion script more generic.  Basically, it
    detects all possible target daemons for ovs-appctl and allows completion
    script to extract subcommands for each type of daemon.
    
    Signed-off-by: Yeqiao Wang <ee07b291@gmail.com>

--- ovs_appctl_compgen.sh
+++ ovs_appctl_compgen.sh
@@ -1,128 +1,192 @@
 #!/bin/bash
-
+#
 # A bash command completion script for ovs-appctl.
-
+#
 # Keywords
 # ========
 #
+#
+#
 # Expandable keywords.
-KWORDS=(bridge port interface dpname br_flow odp_flow)
-# Arguments after keyword expansion.
-ARGS=()
-# Output to the compgen.
-COMP_WORDLIST=
-# Index of the input argument, that, COMP_WORDLIST is targeting.
-COMP_IDX=-1
-# Keyword expanded.
-KWORD_EXPANDED=
+_KWORDS=(bridge port interface dpname br_flow odp_flow)
 # Printf enabler.
-PRINTF_ENABLE=
-# Printf keyword expansion string once.
-PRINTF_KWORD_EXPAND_ONCE=
+_PRINTF_ENABLE=
+# Target in the current completion.
+_APPCTL_TARGET="ovs-vswitchd"
+# Output to the compgen.
+_APPCTL_COMP_WORDLIST=
+
+
 
 # Command Extraction
 # ==================
 #
-# This function parses the ovs-appctl and ovs-vswitchd manpage,
-# for all ovs-appctl sub-commands and generates three files.
 #
-# 1. File containing all full subcommands.
-#    .__ovs_appctl_subcommands.comp
-# 2. File containing all argument combinations of all sub-commands.
-#    .__ovs_appctl_subcmds.comp
-# 3. File containing all options.
-#    .__ovs_appctl_opts.comp
 #
-# This function assumes the ovs-appctl sub-commands will always have
-# 'ovs-appctl module/function ...' format.
+# Extracts all subcommands of daemon.
+# Sub-commands not following the <module>/<action> rule will not be
+# extracted (e.g. exit).  Luckily, for now, 'exit' is the only one.
+# So, add it to output manually.
+extract_subcmds() {
+    local daemon=$_APPCTL_TARGET
+    local subcmds
+
+    subcmds="$(MANWIDTH=2048 man -P cat $daemon | cut -c8- | sed -n \
+	'/^[a-z-]\+\/[a-z-]\+.*$/p' | tr -s ' ' | sed 's/ | /|/g' \
+	| cut -f1 -d ' ')"
+
+    echo "$subcmds exit"
+}
+
+# Extracts all options of ovs-appctl.
+extract_options() {
+    local options
+
+    options="$(MANWIDTH=2048 man -P cat ovs-appctl | cut -c8- | sed -n \
+              '/^\-\-.*$/p' | cut -d ' ' -f1 | cut -d '=' -f1 | sort | uniq)"
+
+    echo "$options"
+}
+
+# Extracts all ovs* commands as possible daemons.
+# The daemon's manpage must contains the "RUNTIME MANAGEMENT COMMANDS"
+# string.
+extract_daemons() {
+    local daemons daemon
+
+    daemons="$(compgen -c ovs | sort | uniq)"
+    # If no change of daemons, use the cache.
+    # Else, recalculate everything.
+    if [ "$_APPCTL_ALL_DAEMONS" != "$daemons" ]; then
+	_APPCTL_TARGET_DAEMONS=()
+	_APPCTL_ALL_DAEMONS="$daemons"
+	for daemon in `compgen -c ovs`; do
+	    local stderr_workaround
+
+	    stderr_workaround="$(MANWIDTH=2048 man -P cat $daemon 2>/dev/null)"
+	    if [ -n "`grep -- "^RUNTIME MANAGEMENT COMMANDS" <<< "$stderr_workaround"`" ]; then
+		_APPCTL_TARGET_DAEMONS+=($daemon)
+	    fi
+	done
+    fi
+}
+
+
+
+# Combination Discovery
+# =====================
 #
-# Note, optional argument will have a leading *.
-extract_subcmd_opts() {
-    local line name arg opts_tmp nlines opt
-    # Tricks the console, so subcommands are all in one line.
-    local cur_cols="`tput cols`"
-    stty cols 1024
+#
+#
+# Given the subcommand formats, finds and returns all combinations.
+subcmd_find_combinations() {
+    local formats="$@"
+    local line combinations
 
-    rm .__ovs_appctl_*.comp 1>&2 2>/dev/null
-    rm .__ovs_appctl_*.tmp 1>&2 2>/dev/null
-    for name in opts subcmds subcommands; do
-	touch .__ovs_appctl_$name.comp
-    done
-
-    man -P cat ovs-appctl | cut -c8- | sed -n \
-	'/^\-\-.*$/p' | cut -d ' ' -f1 | uniq > .__ovs_appctl_opts.comp
-    man -P cat ovs-vswitchd | cut -c8- | sed -n \
-	'/^[a-z]\+\/[a-z]\+.*$/p' | tr -s ' ' | sed 's/ | /|/g' \
-	> .__ovs_appctl_subcommands.comp
-
-    # Finds all combinations of each sub-command.
-    touch .__ovs_appctl_subcmds.tmp
     while read line; do
-	printf "\n" > .__ovs_appctl_subcmds.tmp
+	local arg
+	local combinations_new=
+
 	for arg in $line; do
             # If it is an optional argument, expands the existing
             # combinations.
 	    if [ ! -z "`grep -- \"\[*\]\" <<< \"$arg\"`" ]; then
-		opts_tmp="`sed -n 's/^\[\(.*\)\]$/\1/p' <<< $arg`"
-		IFS='|' read -a opts_arr_tmp <<< "$opts_tmp"
+		local opt_arg="`sed -n 's/^\[\(.*\)\]$/\1/p' <<< $arg`"
+		local opt_args=()
+		local opt nlines
 
-		nlines=`wc -l .__ovs_appctl_subcmds.tmp | cut -d ' ' -f1`
-		for opt in "${opts_arr_tmp[@]}"; do
-		    head -$nlines .__ovs_appctl_subcmds.tmp | \
-			sed "s@\$@ *${opt}@g" >> .__ovs_appctl_subcmds.tmp
+		IFS='|' read -a opt_args <<< "$opt_arg"
+		nlines=`wc -l <<< "$combinations_new"`
+		for opt in "${opt_args[@]}"; do
+		    local combinations_tmp=
+
+		    combinations_tmp="$(head -$nlines <<< "$combinations_new" | \
+			sed "s@\$@ *${opt}@g")"
+		    combinations_new="$(printf "%s\n%s\n" "$combinations_new" "$combinations_tmp")"
 		done
 	    else
                 # Else just appends the argument to the end of each
                 # combination.
-		sed "s@\$@ ${arg}@g" -i .__ovs_appctl_subcmds.tmp
+		combinations_new="$(sed "s@\$@ ${arg}@g" <<< "$combinations_new")"
 	    fi
 	done
-        # Appends the combinations to .__ovs_appctl_subcmds.comp
-	cat .__ovs_appctl_subcmds.tmp >> .__ovs_appctl_subcmds.comp
-    done < .__ovs_appctl_subcommands.comp
+	combinations="$(printf "%s\n%s\n" "$combinations" "$combinations_new")"
+    done <<< "$formats"
 
-    sed -i 's/^ //g' .__ovs_appctl_subcmds.comp
-    rm .__ovs_appctl_*.tmp* 1>&2 2>/dev/null
-    eval stty cols $cur_cols
+    echo "`sed 's/^ //g' <<< "$combinations"`"
 }
 
-# Helpers
-# =======
+
+
+# Helper
+# ======
+#
+#
+#
+# Prints the input to stderr.  $_PRINTF_ENABLE must be filled.
+printf_stderr() {
+    local stderr_out="$@"
+
+    if [ -n "$_PRINTF_ENABLE" ]; then
+	printf "\n$stderr_out" 1>&2
+    fi
+}
+
+# Extracts the bash prompt PS1, output it with the input argument
+# via 'printf_stderr'.
+#
+# Idea inspired by:
+# http://stackoverflow.com/questions/10060500/bash-how-to-evaluate-ps1-ps2
+copy_bash_prompt() {
+    local bash_prompt
+
+    bash_prompt="$(echo want_bash_prompt_PS1 | bash -i 2>&1 \
+	| grep want_bash_prompt_PS1| head -1 | sed 's/ want_bash_prompt_PS1//g')"
+
+    printf_stderr "$bash_prompt $@"
+}
+
+
+
+# Keyword Conversion
+# ==================
+#
+#
 #
 # Converts the argument (e.g. bridge/port/interface name) to the corresponding
 # keyword.
 arg_to_kword() {
-    local word
-    local var="$1"
+    local kword
+    local arg="$1"
 
-    # find all args
-    for word in ${KWORDS[@]}; do
-	> .___arg2kword.tmp
+    for kword in ${_KWORDS[@]}; do
+	local match
+
 	case "$word" in
             bridge|port|interface)
-		ovs-vsctl --columns=name list $word | tr -d ' ' \
+		match="$(ovs-vsctl --columns=name list $kword | tr -d ' ' \
 		    | cut -d ':' -f2 | sed -e 's/^"//' -e 's/"$//' \
-		    | grep "$var" > .___arg2kword.tmp
-		if [ -s .___arg2kword.tmp ]; then
+		    | grep "$arg")"
+		if [ -n "$match" ]; then
 		    # Abbrev of bridge/port/interface.
 		    echo "bpi"
 		    return
 		fi
 		;;
             dpname)
-		if [[ $var =~ ovs-system|ovs-netdev ]]; then
+		if [[ $arg =~ ovs-system|ovs-netdev ]]; then
 		    echo "dpname"
 		    return
 		fi
 		;;
 	    br_flow)
-		if [[ $var =~ "in_port=" ]]; then
+		if [[ $arg =~ "in_port=" ]]; then
 		    echo "br_flow"
 		    return
 		fi
 		;;
 	    odp_flow)
-		if [[ $var =~ "in_port(" ]]; then
+		if [[ $arg =~ "in_port(" ]]; then
 		    echo "odp_flow"
 		    return
 		fi
@@ -132,162 +196,205 @@ arg_to_kword() {
 		;;
 	esac
     done
-    echo "$var"
+
+    # If no matched keyword found, return the input.
+    echo "$arg"
 }
 
 # Expands the keyword to the corresponding instance names.
 kword_to_args() {
-    local word trimmed_word optional is_kword
-    local vars=($@)
+    local kword
+    local input=($@)
     local args=()
 
-    # find all args
-    for word in ${vars[@]}; do
-	optional=
-	is_kword=
+    for kword in ${input[@]}; do
+	local trimmed_kword optional is_kword
+	local new_args=()
 
 	# Cuts the first character is the argument is optional.
-	if [ "${word:0:1}" == "*" ]; then
+	if [ "${kword:0:1}" == "*" ]; then
 	    optional=" (optional)"
-	    trimmed_word="${word:1}"
+	    trimmed_kword="${kword:1}"
 	else
-	    trimmed_word="${word}"
+	    trimmed_kword="${kword}"
 	fi
 
-	case "${trimmed_word}" in
+	case "${trimmed_kword}" in
 	    bridge|port|interface)
-		args=($(ovs-vsctl --columns=name list $trimmed_word \
+		new_args=($(ovs-vsctl --columns=name list $trimmed_kword \
 		    | tr -d ' ' | cut -d ':' -f2 \
 		    | sed -e 's/^"//' -e 's/"$//'))
 		is_kword="kword"
 		;;
 	    dpname)
-		args=(ovs-system ovs-netdev)
+		new_args=(ovs-system ovs-netdev)
 		is_kword="kword"
 		;;
 	    *)
-		args=($trimmed_word)
+		new_args=($trimmed_kword)
 		;;
 	esac
-	ARGS+=( "${args[@]}" )
-	if [ -n "$is_kword" ] && [ -n "$PRINTF_ENABLE" ]; then
-	    printf "\n"
-	    if [ -z "$PRINTF_KWORD_EXPAND_ONCE" ]; then
-		PRINTF_KWORD_EXPAND_ONCE="once"
-		printf "Argument expansion:\n"
+	args+=( ${new_args[@]} )
+	if [ -n "$is_kword" ] && [ -n "$_PRINTF_ENABLE" ]; then
+	    local output_stderr=
+
+	    if [ -z "$printf_expand_once" ]; then
+		printf_expand_once="once"
+		printf -v output_stderr "\nArgument expansion:\n"
 	    fi
-	    printf "    argument keyword%s \"%s\" is expanded to: " "$optional" \
-		$trimmed_word
-	    printf "%s " ${args[@]}
+	    printf -v output_stderr "$output_stderr     argument keyword%s \"%s\" is expanded to: %s " \
+		"$optional" "$trimmed_kword" "${new_args[*]}"
+
+	    printf_stderr "$output_stderr"
 	fi
     done
+
+    echo "${args[@]}"
 }
 
-# Parser
-# ======
-#
-# This script takes the current command line arguments as input,
-# find the command format and returns the possible completions.
-ovs_appctl_comp_helper() {
-    local cmd_line_so_far=($@)
-    local iter=()
-    local subcmd kword args no_opt comp_wordlist line arg
-    local j=-1
 
-    # Extracts the subcmds only when needed.
-    if [ ! -e .__ovs_appctl_subcommands.comp ] \
-	|| [ ! -e .__ovs_appctl_subcmds.comp ] \
-	|| [ ! -e .__ovs_appctl_opts.comp ]; then
-	extract_subcmd_opts
+
+
+# Parse and Compgen
+# =================
+#
+#
+#
+# This function takes the current command line arguments as input,
+# find the command format and returns the possible completions.
+parse_and_compgen() {
+    local subcmd_line=($@)
+    local subcmd=${subcmd_line[0]}
+    local daemon=$_APPCTL_TARGET
+    local subcmd_combinations subcmd_combinations_copy subcmd_format arg
+    local comp_wordlist=""
+
+    # Extracts the subcommand format.
+    subcmd_format="$(MANWIDTH=2048 man -P cat $daemon | cut -c8- | sed -n \
+	    '/^[a-z-]\+\/[a-z-]\+.*$/p' | tr -s ' ' | sed 's/ | /|/g' \
+	    | awk -v opt=$subcmd '$1 == opt {print $0}')"
+    if [ -z "$subcmd_format" ]; then
+	subcmd_format="$subcmd"
     fi
 
-    KWORD_EXPANDED=
-    PRINTF_KWORD_EXPAND_ONCE=
-    ARGS=()
+    # Finds all subcmd combinations.
+    subcmd_combinations="$(subcmd_find_combinations "$subcmd_format")"
 
-    # Tries locating the sub-command.
+    # Now, starts from the first argument, narrows down the
+    # subcommand format combinations.
+    for arg in "${cmd_line_so_far[@]:$j}"; do
+	local kword word
+	local iter=()
+
+	kword="$(arg_to_kword $arg)"
+	if [ "$kword" = "bpi" ]; then
+	    iter=(bridge port interface)
+	else
+	    iter=($kword)
+	fi
+
+	for word in ${iter[@]}; do
+	    local narrow_1 narrow_2
+
+	    narrow_1="$(awk -v opt=$word '$1 == opt {$1=""; print $0}' \
+                        <<< "$subcmd_combinations" | cut -c2-)"
+	    narrow_2="$(awk -v opt=*$word '$1 == opt {$1=""; print $0}' \
+                        <<< "`echo "$subcmd_combinations"`" | cut -c2-)"
+   	    subcmd_combinations_copy="`printf "%s\n%s" "$narrow_1" "$narrow_2" `"
+	done
+	subcmd_combinations="$subcmd_combinations_copy"
+    done
+
+    # Prints subcmd format only when there is match.
+    printf_stderr "`printf "\nCommand format:\n%s" "$subcmd_format"`"
+
+    comp_wordlist="$(kword_to_args `awk '{print $1}' <<< "$subcmd_combinations" | sort | uniq`)"
+
+    echo "$comp_wordlist"
+}
+
+
+
+# Compgen Helper
+# ==============
+#
+#
+#
+# Takes the current command line arguments and returns the possible
+# completions.
+#
+# At beginning, the options are checked and completed.  The function
+# looks for the --target option which gives the target daemon name.
+# If it is not provided, by default, 'ovs-vswitchd' is used.
+#
+# Then, tries to locate and complete the subcommand.  If the subcommand
+# is provided, the following arguments are passed to the 'parse_and_compgen'
+# function to figure out the corresponding completion of the subcommand.
+#
+# Returns the completion arguments on success.
+ovs_appctl_comp_helper() {
+    local cmd_line_so_far=($@)
+    local appctl_target_daemons="${_APPCTL_TARGET_DAEMONS[@]}"
+    local comp_wordlist appctl_subcmd i
+    local j=-1
+
     for i in "${!cmd_line_so_far[@]}"; do
 	if [ $i -le $j ]; then continue; fi
 	j=$i
-	if [ `sed -n '/^--.*$/p' <<< "${cmd_line_so_far[j]}"` ]; then
-            if [ "=" = "${cmd_line_so_far[j+1]}" ]; then
-		((j++))
+	if [ `sed -n '/^--.*$/p' <<< ${cmd_line_so_far[i]}` ]; then
+	    # If --target is found, locate the target daemon.
+	    # Else, it is an option command, fill the comp_wordlist with
+	    # all options.
+	    if [ "${cmd_line_so_far[i]}" == "--target" ]; then
 		if [ -n "${cmd_line_so_far[j+1]}" ]; then
-		    no_opt="no_opt"
-		    ((j++))
+		    local daemon
+
+		    for daemon in ${appctl_target_daemons[@]}; do
+			# Greps "$daemon" in argument, since the argument may
+			# be the path to the pid file.
+			if [ `grep -- "$daemon" <<< "${cmd_line_so_far[j+1]}"` ]; then
+			    _APPCTL_TARGET="$daemon"
+			    ((j++))
+			    break
+			fi
+		    done
+		    continue
 		else
-		    KWORD_EXPANDED="true"
-		    COMP_WORDLIST="`grep -- "${cmd_line_so_far[j-1]}" \
-                        __ovs_appctl_opts.comp | cut -d '=' -f2`"
-		    if [ -n "$PRINTF_ENABLE" ]; then
-			printf "\n"
-			printf "Option takes in: %s" $COMP_WORDLIST
-		    fi
-		    return
+		    comp_wordlist="$appctl_target_daemons"
+		    break
 		fi
-            fi
-	    continue
+	    else
+		comp_wordlist="$(extract_options)"
+		break
+	    fi
 	fi
-	subcmd="${cmd_line_so_far[i]}"
+	# Takes the first non-option argument as subcmd.
+	appctl_subcmd="${cmd_line_so_far[i]}"
 	break
     done
 
-    # If subcommand is not found, should present all available completions.
-    # Otherwise, start parsing the input arguments.
-    if [ -z "$subcmd" ]; then
-        # If no comment, all options/subcmds are available.
-	if [ -z "$no_opt" ]; then
-	    comp_wordlist="`cat .__ovs_appctl_*.comp | cut -d ' ' -f1 | \
-                        sed 's/=.*$/=/'`"
+    if [ -z "$comp_wordlist" ]; then
+        # If the subcommand is not found, provides all subcmds.
+        # Else parses the current arguments and finds the possible completions.
+	if [ -z "$appctl_subcmd" ]; then
+	    comp_wordlist="$(extract_subcmds) $(extract_options)"
 	else
-	    comp_wordlist="`cat .__ovs_appctl_subcmds.comp | cut -d ' ' -f1`"
+            # $j stores the index of the subcmd in cmd_line_so_far.
+	    comp_wordlist="$(parse_and_compgen "${cmd_line_so_far[@]:$j}")"
 	fi
-    else
-	if [ -n "$PRINTF_ENABLE" ]; then
-	    printf "\n"
-	    printf "Command format:\n"
-	    while read -r line
-	    do
-		printf "    %s\n" "$line"
-	    done <<< "`awk -v opt=$subcmd '$1 == opt {print $0}' \
-                       .__ovs_appctl_subcommands.comp`"
-	fi
-
-	KWORD_EXPANDED="true"
-	cp .__ovs_appctl_subcmds.comp .___tmp.tmp
-
-        # $j stores the index of the subcmd in cmd_line_so_far.
-	# Now, starts from the first argument, narrows down the
-	# subcommand format combinations.
-	for arg in "${cmd_line_so_far[@]:$j}"; do
-	    > .___tmp.tmp.tmp
-
-	    kword=$(arg_to_kword $arg)
-	    if [ "$kword" = "bpi" ]; then
-		iter=(bridge port interface)
-	    else
-		iter=($kword)
-	    fi
-
-	    for word in ${iter[@]}; do
-		awk -v opt=$word '$1 == opt {$1=""; print $0}' \
-		    .___tmp.tmp | cut -c2- >> .___tmp.tmp.tmp
-		awk -v opt=*$word '$1 == opt {$1=""; print $0}' \
-		    .___tmp.tmp | cut -c2- >> .___tmp.tmp.tmp
-	    done
-	    cat .___tmp.tmp.tmp > .___tmp.tmp
-	done
-
-	kword_to_args `awk '{print $1}' .___tmp.tmp | sort | uniq`
-	comp_wordlist="${ARGS[@]}"
     fi
-    COMP_WORDLIST="$comp_wordlist"
+
+    echo "$comp_wordlist"
 }
+
+
 
 export LC_ALL=C
 
 # Compgen
 # =======
+#
+#
 #
 # The compgen function.
 _ovs_appctl_complete() {
@@ -298,34 +405,34 @@ _ovs_appctl_complete() {
 
   # Do not print anything at first [TAB] execution.
   if [ "$COMP_TYPE" -eq "9" ]; then
-      PRINTF_ENABLE=""
+      _PRINTF_ENABLE=""
   else
-      PRINTF_ENABLE="enabled"
+      _PRINTF_ENABLE="enabled"
   fi
 
-  # Important to skip unwanted printf to stdout.
-  if [ -z "$cur" ] || [ $COMP_CWORD -ne $COMP_IDX ]; then
-      ovs_appctl_comp_helper ${COMP_WORDS[@]:1:COMP_CWORD-1}
-      COMP_IDX=$COMP_CWORD
+  # Checks for any change of daemons.  If recalculation needed, the execution
+  # may take several seconds.
+  extract_daemons
+
+  # Invokes the helper function to get all available completions.
+  _APPCTL_COMP_WORDLIST=$(ovs_appctl_comp_helper \
+      ${COMP_WORDS[@]:1:COMP_CWORD-1})
+
+  # Prints all available completions to stderr.  If there is only one matched
+  # completion, do nothing.
+  if [ -n "$_PRINTF_ENABLE" ] \
+      && [ -n "`echo $_APPCTL_COMP_WORDLIST | tr ' ' '\n' | grep -- "^$cur"`" ]; then
+      printf_stderr "\nAvailable completions:\n"
   fi
 
-  # If there is only one expanded keyword, and the keyword expands to only
-  # one instance name, the completion will fill in the name automatically.
-  # This caused a confusing display in the output (i.e. a new line with only
-  # the name).  To fix this, we will print a fake comand line.
-  # Otherwise, prints the help message to separate completions.
-  if [ -n "$KWORD_EXPANDED" ] \
-      && [ "`echo $COMP_WORDLIST | tr ' ' '\n' | wc -l`" -le "1" ] \
-      && [ -n "$PRINTF_ENABLE" ] ; then
-      printf "\n$USER@$HOSTNAME:$PWD#"
-      printf -- ' %s' "${COMP_WORDS[@]}"
-  elif [ -n "$PRINTF_ENABLE" ] \
-      && [ -n "`echo $COMP_WORDLIST | tr ' ' '\n' | grep -- "^$cur"`" ]; then
-      printf "\n\n"
-      printf "Available completions:\n"
+  # If there is only one available completion, need to print the new bash
+  # prompt and copy the ${COMP_WORDS[@]}, .
+  if [ -n "$_PRINTF_ENABLE" ] \
+      && [ "`echo $_APPCTL_COMP_WORDLIST | tr ' ' '\n' | wc -l`" -eq "1" ]; then
+      copy_bash_prompt "${COMP_WORDS[@]}"
   fi
 
-  COMPREPLY=( $(compgen -W "`echo $COMP_WORDLIST | tr ' ' '\n' | sort \
+  COMPREPLY=( $(compgen -W "`echo $_APPCTL_COMP_WORDLIST | tr ' ' '\n' | sort \
                              | uniq`" -- $cur) )
 
   return 0
